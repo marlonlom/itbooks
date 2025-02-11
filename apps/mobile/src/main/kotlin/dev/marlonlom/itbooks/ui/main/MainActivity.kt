a50825665b4733c28a391fe5a6cdf5c2050e5e1f
Marlon López <malm.gm@gmail.com>
2025-02-11 16:36:18 -0500

    feat(apps:mobile): Enable edge to edge in main activity, remove unused theme colors

--- apps/mobile/src/main/kotlin/dev/marlonlom/itbooks/ui/main/MainActivity.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/itbooks/ui/main/MainActivity.kt
@@ -4,23 +4,30 @@
  */
 package dev.marlonlom.itbooks.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.marlonlom.itbooks.ui.theme.ITBooksTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
+    enableEdgeToEdge(
+      statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+    )
     setContent {
       ITBooksTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -38,7 +45,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
   Text(
     text = "Hello $name!",
-    modifier = modifier,
+    modifier = modifier.fillMaxWidth().padding(20.dp),
+    textAlign = TextAlign.Center,
   )
 }
 
