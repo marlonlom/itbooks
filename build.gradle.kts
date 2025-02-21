39cf69b5e9c4ef3738acb7fbf50d18c30124b9a3
Marlon López <malm.gm@gmail.com>
2025-02-21 17:08:42 -0500

    feat: add ksp and android library plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
   alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.google.devtools.ksp) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.spotless) apply false
