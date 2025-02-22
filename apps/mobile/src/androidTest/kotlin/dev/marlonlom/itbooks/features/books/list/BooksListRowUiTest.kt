e162fa9c82cbda389aadd934cdb283a63f50dfb5
Marlon López <malm.gm@gmail.com>
2025-02-22 18:12:12 -0500

    feat: Add android ui tests for books list feature

---
+++ apps/mobile/src/androidTest/kotlin/dev/marlonlom/itbooks/features/books/list/BooksListRowUiTest.kt
@@ -0,0 +1,49 @@
+package dev.marlonlom.itbooks.features.books.list
+
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class BooksListRowUiTest {
+
+  @get:Rule
+  val composeTestRule = createComposeRule()
+
+  @Test
+  fun shouldClickBooksListRow() {
+    with(composeTestRule) {
+      val book = BooksListItem(
+        isbn13 = "9781491954249",
+        title = "Designing Across Senses",
+        price = "$27.59",
+        picture = "https://itbook.store/img/books/9781491954249.png",
+      )
+      var clicked = false
+      var isbn13 = ""
+      setContent {
+        BooksListRow(
+          book = book,
+          onBookListItemClicked = { txt ->
+            clicked = true
+            isbn13 = txt
+          },
+        )
+      }
+      onNodeWithText(book.title).isDisplayed()
+      onNodeWithText(book.price).isDisplayed()
+      onNodeWithContentDescription("Book picture for ${book.title}").isDisplayed()
+      onNodeWithTag("book_list_item_row_${book.isbn13}").performClick()
+      Truth.assertThat(clicked).isTrue()
+      Truth.assertThat(isbn13).isEqualTo(book.isbn13)
+    }
+  }
+}
