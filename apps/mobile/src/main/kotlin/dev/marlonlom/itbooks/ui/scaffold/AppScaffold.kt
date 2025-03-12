00f8c5f97d6a60ba1178a44630a9d5bed3204d96
Marlon López <malm.gm@gmail.com>
2025-03-12 11:09:48 -0500

    feat(apps:mobile): Update mobile app scaffold ui

--- apps/mobile/src/main/kotlin/dev/marlonlom/itbooks/ui/scaffold/AppScaffold.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/itbooks/ui/scaffold/AppScaffold.kt
@@ -7,21 +7,17 @@ package dev.marlonlom.itbooks.ui.scaffold
 import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.layout.AnimatedPane
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
-import androidx.compose.material3.adaptive.layout.PaneAdaptedValue
 import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import dev.marlonlom.itbooks.features.books.detail.BookDetailsScreenPane
 import dev.marlonlom.itbooks.features.books.list.BooksListScreen
 
 /**
@@ -38,11 +34,6 @@ fun AppScaffold() {
     navigator.navigateBack()
   }
 
-  val detailBgModifier = when (navigator.scaffoldValue.secondary) {
-    PaneAdaptedValue.Hidden -> Modifier.background(MaterialTheme.colorScheme.surface)
-    else -> Modifier.background(MaterialTheme.colorScheme.surfaceVariant, MaterialTheme.shapes.large)
-  }
-
   ListDetailPaneScaffold(
     modifier = Modifier
       .fillMaxSize()
@@ -57,7 +48,6 @@ fun AppScaffold() {
             Log.d("AppScaffold", "Settings icon clicked.")
           },
           onBookListItemClicked = { isbn13 ->
-            Log.d("AppScaffold", "Book list item [$isbn13] Selected.")
             navigator.navigateTo(
               pane = ListDetailPaneScaffoldRole.Detail,
               content = ItBookNavigationItem(isbn13),
@@ -67,21 +57,16 @@ fun AppScaffold() {
       }
     },
     detailPane = {
-      Box(
-        modifier = Modifier
-          .fillMaxSize()
-          .then(detailBgModifier),
-        contentAlignment = Alignment.Center,
-      ) {
-        navigator.currentDestination?.content?.let { item ->
-          Text(
-            modifier = Modifier.fillMaxSize(),
-            textAlign = TextAlign.Center,
-            text = "Details for $item",
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-          )
-        }
-      }
+      val itBookNavigationItem = navigator.currentDestination?.content
+      BookDetailsScreenPane(
+        bookItem = itBookNavigationItem,
+        detailPaneAdaptedValue = navigator.scaffoldValue.secondary,
+        onBack = {
+          if (navigator.canNavigateBack()) {
+            navigator.navigateBack()
+          }
+        },
+      )
     },
   )
 }
