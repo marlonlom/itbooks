b080996802ea239bfc944a88ec4eb235f3077a26
Marlon López <malm.gm@gmail.com>
2025-03-13 16:35:48 -0500

    feat(apps:mobile): Update android ui tests for books listing feature

--- apps/mobile/src/androidTest/kotlin/dev/marlonlom/itbooks/features/books/list/BooksEmptyListMessageUiTest.kt
+++ apps/mobile/src/androidTest/kotlin/dev/marlonlom/itbooks/features/books/list/BooksEmptyListMessageUiTest.kt
@@ -5,7 +5,6 @@
 package dev.marlonlom.itbooks.features.books.list
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
