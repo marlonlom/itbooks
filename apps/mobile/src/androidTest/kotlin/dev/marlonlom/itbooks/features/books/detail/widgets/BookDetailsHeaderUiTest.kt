2aec1f94bb5407b78f5ecf30227dec4a7af6f719
Marlon López <malm.gm@gmail.com>
2025-03-16 01:42:29 -0500

    refactor(apps:mobile): Update android ui tests for book details header composable

--- apps/mobile/src/androidTest/kotlin/dev/marlonlom/itbooks/features/books/detail/widgets/BookDetailsHeaderUiTest.kt
+++ apps/mobile/src/androidTest/kotlin/dev/marlonlom/itbooks/features/books/detail/widgets/BookDetailsHeaderUiTest.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import dev.marlonlom.itbooks.features.books.detail.BookDetailsItem
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,10 +27,27 @@ internal class BookDetailsHeaderUiTest {
       var buyButtonTxt = ""
       var shareButtonTxt = ""
       var backButtonClicked = false
-      val bookIsbn13 = "9781098111878"
+      val bookDetailsItem = BookDetailsItem(
+        isbn13 = "9781098111878",
+        isbn10 = "1098111877",
+        title = "Data Visualization with Python and JavaScript, 2nd Edition",
+        subtitle = "Scrape, Clean, Explore, and Transform Your Data",
+        detail = "How do you turn raw, unprocessed, or malformed data into dynamic, " +
+          "interactive web visualizations? In this practical book, author Kyran Dale " +
+          "shows data scientists and analysts-as well as Python and JavaScript developers-how " +
+          "to create the ideal toolchain for the job. By providing engaging examples and...",
+        authors = "Kyran Dale",
+        publisher = "O'Reilly Media",
+        language = "English",
+        pages = 566,
+        year = 2022,
+        rating = 4,
+        price = "$60.99",
+        picture = "https://itbook.store/img/books/9781098111878.png",
+      )
       setContent {
         BookDetailsHeader(
-          bookIsbn13 = { bookIsbn13 },
+          selectedBook = { bookDetailsItem },
           isBackButtonVisible = { true },
           onBack = {
             backButtonClicked = true
@@ -44,11 +62,11 @@ internal class BookDetailsHeaderUiTest {
       }
       onNodeWithTag("book_detail_buy_btn").assertIsDisplayed()
       onNodeWithTag("book_detail_buy_btn").performClick()
-      assertThat(buyButtonTxt).endsWith(bookIsbn13)
+      assertThat(buyButtonTxt).endsWith(bookDetailsItem.isbn13)
 
       onNodeWithTag("book_detail_share_btn").assertIsDisplayed()
       onNodeWithTag("book_detail_share_btn").performClick()
-      assertThat(shareButtonTxt).isEqualTo(bookIsbn13)
+      assertThat(shareButtonTxt).contains(bookDetailsItem.title)
 
       onNodeWithTag("book_detail_back_btn").assertIsDisplayed()
       onNodeWithTag("book_detail_back_btn").performClick()
