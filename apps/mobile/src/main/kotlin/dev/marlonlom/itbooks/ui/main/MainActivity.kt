50d4177791a7c281fe6880555527bd65f6bc96de
Marlon López <malm.gm@gmail.com>
2025-03-16 00:44:53 -0500

    feat(apps:mobile): Integrate preferences repository in main activity; Implement main content composable

--- apps/mobile/src/main/kotlin/dev/marlonlom/itbooks/ui/main/MainActivity.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/itbooks/ui/main/MainActivity.kt
@@ -8,8 +8,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import dev.marlonlom.itbooks.ui.scaffold.AppScaffold
-import dev.marlonlom.itbooks.ui.theme.ITBooksTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.compose.KoinContext
 
@@ -25,12 +32,19 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    var mainActivityUiState: MainActivityUiState by mutableStateOf(MainActivityUiState.Loading)
+
+    lifecycleScope.launch {
+      lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+        mainActivityViewModel.uiState.onEach { mainActivityUiState = it }.collect()
+      }
+    }
+
     enableEdgeToEdge()
     setContent {
-      ITBooksTheme {
-        KoinContext {
-          AppScaffold()
-        }
+      KoinContext {
+        MainContent(mainActivityUiState)
       }
     }
   }
