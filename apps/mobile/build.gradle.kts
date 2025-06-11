874a0ea244b914b9a117f99b25ae41d8258198d0
Marlon López <malm.gm@gmail.com>
2025-06-10 22:48:49 -0500

    refactor(apps:mobile): update gradle build, apply mobile app convention plugin

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -3,45 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
-  alias(libs.plugins.kotlin.compose)
+  id("itbooks.android.application.mobile")
+  id("itbooks.android.app.compose.mobile")
+  id("itbooks.android.coil")
+  id("itbooks.android.koin")
+  id("itbooks.spotless")
   alias(libs.plugins.kotlin.parcelize)
-}
-
-android {
-  namespace = "dev.marlonlom.itbooks"
-  compileSdk = 35
-
-  defaultConfig {
-    applicationId = "dev.marlonlom.itbooks"
-    minSdk = 24
-    targetSdk = 35
-    versionCode = 1
-    versionName = "1.0.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro",
-      )
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_17.majorVersion
-  }
-  buildFeatures {
-    compose = true
-  }
 }
 
 dependencies {
