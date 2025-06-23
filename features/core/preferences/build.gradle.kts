518cf7aa2627106bdfe639ef9f6c9d347c5b8d3e
Marlon López <malm.gm@gmail.com>
2025-06-23 18:17:04 -0500

    refactor(core:preferences): apply convention plugin

--- features/core/preferences/build.gradle.kts
+++ features/core/preferences/build.gradle.kts
@@ -3,58 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 plugins {
-  alias(libs.plugins.android.library)
-  alias(libs.plugins.kotlin.android)
+  id("itbooks.android.library")
+  id("itbooks.android.lib.compose")
+  id("itbooks.android.koin")
+  id("itbooks.spotless")
 }
 
 android {
   namespace = "dev.marlonlom.itbooks.core.preferences"
-  compileSdk = 35
-
-  defaultConfig {
-    minSdk = 24
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro",
-      )
-    }
-  }
-
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-
-  kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_17.majorVersion
-  }
 }
 
 dependencies {
-
-  val koinBom = platform(libs.koin.bom)
-  implementation(koinBom)
-
-  implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.datastore.preferences)
-  implementation(libs.bundles.koin)
-
-  testImplementation(libs.junit)
-  testImplementation(libs.kotlinx.coroutines.test)
-
-  androidTestImplementation(libs.androidx.espresso.core)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.ui.test.junit4)
-  androidTestImplementation(libs.google.truth)
-
-  debugImplementation(libs.androidx.ui.test.manifest)
-  debugImplementation(libs.androidx.ui.tooling)
 }
